2511e4c5664e33bcd6ec73f1ff5c0ca3e0bd45a2
RahulDev <rahul.j.india@gmail.com>
2020-04-27 16:44:40 +0530

    Final changes according to feedback received (maiil)

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -16,22 +16,41 @@ import java.util.HashMap;
 public class AnalyticsCounter {
 
 
-	 private HashMap<String,Integer> symptomsWithOccurrence ;
-	 private ArrayList <String> sortedSymptoms;
+	private HashMap<String,Integer> symptomsWithOccurrence ;
+	private ArrayList <String> fileAsArrayList;
+	private ArrayList <String> sortedSymptoms;
 
 
 	 AnalyticsCounter (){
 		 symptomsWithOccurrence = new HashMap<>();
 		 sortedSymptoms = new ArrayList<>();
+		 fileAsArrayList = new ArrayList<>();
+
 	 }
+
+
+	 /**
+	 *
+	 * @throws IOException
+	 * Read The contents of the Input file and stores the into an Arraylist
+	 *
+	 */
+	void readSymptomsFile() throws IOException {
+		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile(
+				"../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse" +
+						"/symptoms.txt");
+		fileAsArrayList = (ArrayList<String>) readSymptomDataFromFile.getSymptoms();
+		countSymptomsAlphabetically(fileAsArrayList);
+
+	}
+
 
 	/***
 	 *
 	 * @param inputFromFile It contains the input from the file
 	 * @throws IOException
 	 * The method takes the ArrayList as an input which will have all the contents of file.
-	 * The Contents then parsed and sorted according to requirement and again copied
-	 * to an ArrayList, to be written to an output file.
+	 * The Contents then parsed and stored in an ArrayList.
 	 *
 	 */
 	void countSymptomsAlphabetically(ArrayList inputFromFile) throws IOException {
@@ -49,13 +68,24 @@ public class AnalyticsCounter {
 			}
 
 		}
-
 		symptomsWithOccurrence.forEach((k,v)->sortedSymptoms.add(k +" "+ ":" + " "+v));
+		sortingList();
+
+	}
+
+
+	/**
+	 *
+	 * @throws IOException
+	 * Sort the contents of the ArrayList in alphabetical order
+	 */
+	void sortingList() throws IOException {
+
 		Collections.sort(sortedSymptoms);
 		writtenOutput();
 
-
 	}
+
 
 	/***
 	 *
@@ -73,6 +103,7 @@ public class AnalyticsCounter {
 
 	}
 
+
 	/**
 	 *
 	 * @param args
@@ -80,19 +111,9 @@ public class AnalyticsCounter {
 	 */
 	public static void main(String args[]) throws IOException {
 
-		ArrayList <String> FileAsArrayList;
-
-		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile(
-				"../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse" +
-				"/symptoms.txt");
-
-		FileAsArrayList = (ArrayList<String>) readSymptomDataFromFile.getSymptoms();
-
 		AnalyticsCounter analyticsCounter= new AnalyticsCounter();
 
-		analyticsCounter.countSymptomsAlphabetically(FileAsArrayList);
-
-
+		analyticsCounter.readSymptomsFile();
 
 	}
 
